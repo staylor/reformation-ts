600f2aeb148802ab2ec84dd7a44f833b5abb6a44
Scott Taylor <scott.c.taylor@mac.com>
2018-03-15 14:19:49 -0400

    ensure that build size does not explode

--- src/client/base.tsx
+++ src/client/base.tsx
@@ -4,10 +4,18 @@ import { hydrate } from 'emotion';
 import { HelmetProvider } from 'react-helmet-async';
 import { BrowserRouter } from 'react-router-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import enLocaleData = require('react-intl/locale-data/en');
+import esLocaleData = require('react-intl/locale-data/es');
 
 export default function Base({ children }) {
   const locale = window['__l10n'].locale;
-  const localeData = require(`react-intl/locale-data/${locale}`);
+  let localeData;
+  if (locale === 'es') {
+    localeData = esLocaleData;
+  } else {
+    localeData = enLocaleData;
+  }
+
   addLocaleData(localeData);
 
   hydrate(window['__emotion']);
